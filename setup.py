058e757b1f735c629e8d41e5d8213f3931768144
Mads Andreasen <github@andreasen.cc>
2021-02-21 09:28:34 +0100

    Updated setup after renaming

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup
 
 setup(name='PySideFlask',
-      version='1.1',
+      version='2.0',
       setup_requires=['setuptools-markdown'],
       long_description_markdown_filename='README.md',
+      long_description_content_type = 'text/markdown',
       description='Create desktop application by using Flask and QtWebEngine',
-      url="https://github.com/smoqadam/PySideFlask",
-      author='​Saeed Moqadam, Ezequiel Castaño​',
-      author_email='saeed.moqadam@gmail.com, castanoezequielleonardo@gmail.com',
+      url="https://github.com/MadsAndreasen/PySideFlask",
+      author='Mads Andreasen',
+      author_email='app@andreasen.cc',
       license='MIT',
       install_requires=[
           'flask',
           'pyside2',
       ],
-      packages=['PySideFlask'],
+      packages=['pysideflask'],
       zip_safe=False,
-      keywords = ['​GUI​', '​Flask​', 'Qt'],
+      keywords = ['​GUI​', '​Flask​', 'Qt', 'webview'],
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Developers',
